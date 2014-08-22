78b159a6c4ee47a4a622c19673541d864ed4c19d
Andre Farzat <andrefarzat@gmail.com>
2014-08-22 00:05:05 -0400

    adding django_nose into INSTALLED_APPS

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd libraries
+    'django_nose',
+    # Local
     'codetask',
 )
 
