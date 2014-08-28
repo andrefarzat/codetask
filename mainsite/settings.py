d92d34262c5b761146c6bb868afaf83bc59ede6d
Andre Farzat <andrefarzat@gmail.com>
2014-08-27 23:36:32 -0400

    adding STATIC_ROOT

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -85,5 +85,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 TEST_RUNNER = 'mainsite.MainSiteNoseTestSuiteRunner'
