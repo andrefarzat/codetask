2083106d5685cc67f58b96569d0bd3f95bf98153
Andre Farzat <andrefarzat@gmail.com>
2014-08-27 23:40:22 -0400

    adding STATIC_ROOT

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -1,12 +1,11 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'codetask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
